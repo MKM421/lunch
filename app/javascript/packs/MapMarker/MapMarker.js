3283f00edbd8fd5aa91ca2272619ff1ee720a0a8
Michael Morgan <morganmichael340@gmail.com>
2021-04-21 00:05:29 -0700

    add custom map markers
    show info window for each marker

---
+++ app/javascript/packs/MapMarker/MapMarker.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './MapMarker.css'
+
+class MapMarker extends React.Component {
+  render() {
+    return (
+      <div className="map-marker"></div>
+    );
+  }
+}
+
+export default MapMarker;
